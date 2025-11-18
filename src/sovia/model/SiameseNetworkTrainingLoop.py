a0dbe868b15e852742ee6d428706ac71c4997ba8
s.janssen <s.janssen@prosoz.de>
2025-11-18 09:26:08 +0100

    batch size verringern und inmem cache disablen

--- src/sovia/model/SiameseNetworkTrainingLoop.py
+++ src/sovia/model/SiameseNetworkTrainingLoop.py
@@ -49,7 +49,7 @@ class ContrastiveLoss(nn.Module):
 @dataclass
 class TrainingConfig:
     num_epochs: int = 100
-    batch_size: int = 512
+    batch_size: int = 256
     learning_rate: float = 1e-4
     distance_threshold: float = 0.5
     rotate = False
